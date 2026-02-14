5437effd9b4ce0cf868610d443c5d2f847b3d45b
Arpit Arora <aroraarpit2806@gmail.com>
2026-02-13 21:42:52 -0800

    Add fast_sims subpackage: particle spray and restricted N-body
    - _common.py: shared helpers (dynamical friction, orbit integration,
      progenitor potential builders, _create_perturber_potential)
    - spray.py: create_particle_spray_stream with Chen+2025 and Fardal+2015
      IC methods, time_stripping parameter for custom release times
    - restricted.py: run_restricted_nbody with add_perturber support
    - 12 tests using MWPotential22 (all passing)

--- nbody_streams/fast_sims/__init__.py
+++ nbody_streams/fast_sims/__init__.py
@@ -7,16 +7,20 @@ techniques.  All methods use Agama for the host potential.
 Usage
 -----
 >>> from nbody_streams import fast_sims
->>> fast_sims.particle_spray(...)
->>> fast_sims.restricted_nbody(...)
+>>> fast_sims.create_particle_spray_stream(...)
+>>> fast_sims.run_restricted_nbody(...)
 """
 
-# Re-export public API so users get a flat namespace:
-#   nbody_streams.fast_sims.particle_spray(...)
-# rather than:
-#   nbody_streams.fast_sims.spray.particle_spray(...)
+from .spray import (
+    create_ic_particle_spray_chen2025,
+    create_ic_particle_spray_fardal2015,
+    create_particle_spray_stream,
+)
+from .restricted import run_restricted_nbody
 
-# from .spray import ...        # TODO: uncomment once implemented
-# from .restricted import ...   # TODO: uncomment once implemented
-
-__all__ = []  # TODO: populate as functions are added
+__all__ = [
+    "create_ic_particle_spray_chen2025",
+    "create_ic_particle_spray_fardal2015",
+    "create_particle_spray_stream",
+    "run_restricted_nbody",
+]
